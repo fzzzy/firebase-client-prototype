cc8ee8ed39d3e674444a79b66e9a3b5a57768e37
Donovan Preston <donovanpreston@gmail.com>
2016-04-25 11:57:48 -0400

    Use redux

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ import "babel-polyfill";
 import { FirebaseClient } from "./client";
 import * as React from "react";
 import { render, findDOMNode } from "react-dom";
+import { createStore } from "redux";
 
 let baseUrl = 'https://hello-async.firebaseio.com/test';
+let _client;
+let store = null;
 
-function draw(state) {
+function draw() {
+  let state = store.getState();
   function send_chat(obj) {
     state.client.post("", obj);
   }
@@ -46,39 +50,51 @@ class Screen extends React.Component {
   }
 }
 
-class State {
-  constructor(client) {
-    this.title = "Room title";
-    this.chat = [];
-    this.client = client;
-  }
-
-  pushChat(chat) {
-    this.chat.push(chat);
-    draw(this);
-  }
-
-  setTitle(title) {
-    this.title = title;
-    draw(this);
+function createReducer(client) {
+  return function reduxStore(state, action) {
+    if (typeof state === "undefined") {
+      return {
+        title: "Room title",
+        chat: [],
+        client: client
+      }
+    }
+    switch (action.type) {
+      case "PUSH_CHAT":
+        let chat = JSON.parse(JSON.stringify(state.chat));
+        chat.push(action.chat);
+        return {
+          title: state.title,
+          chat: chat,
+          client: state.client
+        };
+      case "SET_TITLE":
+        return {
+          title: action.title,
+          chat: state.chat,
+          client: state.client
+        }
+    }
   }
 }
 
 function main() {
   let client = new FirebaseClient(baseUrl);
-  let state = new State(client);
-  draw(state);
+  store = createStore(createReducer(client));
+  store.subscribe(draw);
   client.on("put", (val) => {
     if (val.data) {
       if (val.path === "/") {
         for (let index in val.data) {
           let el = val.data[index];
           if (el.chat) {
-            state.pushChat(el.chat);
+            store.dispatch(
+              {type: "PUSH_CHAT", chat: el.chat});
           }
         }
       } else if (val.data.chat) {
-        state.pushChat(val.data.chat);
+        store.dispatch(
+          {type: "PUSH_CHAT", chat: val.data.chat});
       }
     }
     console.log("put", val);
